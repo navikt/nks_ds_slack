52b0f717af8f7b53a26f1c141302168c39fe4574
Jørgen Nordmoen <jorgen@nordmoen.net>
2024-06-04 13:26:42 +0200

    La til 'lint' gruppe i pyproject.toml

--- noxfile.py
+++ noxfile.py
@@ -5,16 +5,47 @@ Nox files are an easy way to automate testing in different virtual environments.
 will automatically create virtual environments and install required packages.
 """
 
+from typing import Iterable
+
 import nox
 
 # Default sessions to run when just `nox` is run in the project
 nox.options.sessions = ["lint"]
 
 
+# Lånt fra: https://github.com/cjolowicz/cookiecutter-hypermodern-python-instance/blob/90e45cee4725c51c77864ab21efead23d5226306/noxfile.py#L26-L51
+def install(session: nox.Session, *, groups: Iterable[str], root: bool = True) -> None:
+    """Install the dependency groups using Poetry.
+
+    This function installs the given dependency groups into the session's
+    virtual environment. When ``root`` is true (the default), the function
+    also installs the root package and its default dependencies.
+
+    To avoid an editable install, the root package is not installed using
+    ``poetry install``. Instead, the function invokes ``pip install .``
+    to perform a PEP 517 build.
+
+    Args:
+        session: The Session object.
+        groups: The dependency groups to install.
+        root: Install the root package.
+    """
+    session.run_always(
+        "poetry",
+        "install",
+        "--no-root",
+        "--sync",
+        "--{}={}".format("only" if not root else "with", ",".join(groups)),
+        external=True,
+    )
+    if root:
+        session.install(".")
+
+
 @nox.session  # type: ignore[misc]
 def lint(session: nox.Session) -> None:
     """Run pre-commit"""
-    session.install("pre-commit")
+    install(session, groups=["lint"], root=False)
     session.run("pre-commit", "run", "--all-files", "--show-diff-on-failure")
 
 
@@ -23,7 +54,7 @@ def fix(session: nox.Session) -> None:
     """
     Run ruff to fix errors and format, then run linters
     """
-    session.install("ruff")
+    install(session, groups=["lint"], root=False)
     session.run("ruff", "check", "--fix", ".")
     session.run("ruff", "format", ".")
     # Run linting after fixing to warn about mypy
